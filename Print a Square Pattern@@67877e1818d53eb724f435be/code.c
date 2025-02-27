f39c3290924ab24415eccf8e2767a719af7bbde0
Vranda1289 <wadhvavranda@gmail.com>
2025-02-27 10:42:03 +0530

    Solution to Print a Square Pattern

---
+++ Print a Square Pattern@@67877e1818d53eb724f435be/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int r,c,row,column;
+scanf("%d%d",&row,&column);
+for(r=1; r<=row; r++){
+    for(c=1; c<=column; c++ ){
+        printf("*",r,c);
+    }
+    printf("\n");
+}
+
+
+  return 0;
+
+}
